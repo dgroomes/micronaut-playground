96328a8584b7e05ecef9d0a9e9a118c985e66762
David Groomes <dwgroomes@gmail.com>
2021-09-26 17:49:23 -0500

    Illustrate an ad hoc query by way of a "word count"

--- sql/src/main/java/dgroomes/sql/SqlApplication.java
+++ sql/src/main/java/dgroomes/sql/SqlApplication.java
@@ -2,9 +2,12 @@ package dgroomes.sql;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.context.env.Environment;
+import io.micronaut.transaction.jdbc.DelegatingDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,14 +20,17 @@ public class SqlApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SqlApplication.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var builder = ApplicationContext.builder(SqlApplication.class, Environment.CLI);
         try (var ctx = builder.build()) {
+            log.info("Starting the application...\n\n");
             ctx.start();
             var repo = ctx.getBean(ObservationsRepository.class);
+            log.info("\n\n");
 
             makeObservations(repo);
             listObservations(repo);
+            adhocQuery(ctx);
             log.info("\n\n");
         }
     }
@@ -53,6 +59,7 @@ public class SqlApplication {
      * @param repo a Micronaut repository object over the "observations" table
      */
     private static void makeObservations(ObservationsRepository repo) {
+        log.info("Creating some observations and inserting them into the database...\n\n");
         var items = randomize(List.of("Java", "Micronaut", "SQL")).iterator();
         var descriptors = randomize(List.of("is the best!", "is amazing!", "rocks!")).iterator();
 
@@ -62,6 +69,7 @@ public class SqlApplication {
                 String.format("%s %s", items.next(), descriptors.next()));
 
         repo.save(new Observation("random software observations", notes));
+        log.info("\n\n");
     }
 
     /**
@@ -70,12 +78,58 @@ public class SqlApplication {
      * @param repo a Micronaut repository object over the "observations" table
      */
     private static void listObservations(ObservationsRepository repo) {
+        log.info("Listing all of the observations in the 'observations' table...\n\n");
 
         var observations = repo.findAll();
-        log.info("\n\n");
-        log.info("Found the following observations...");
+        log.info("Found the following observations:\n");
         for (Observation observation : observations) {
             log.info(observation.toString());
         }
+        log.info("\n\n");
+    }
+
+    /**
+     * Do a "word count" for the words contained in the "notes" column across all rows in the "observations" table.
+     * <p>
+     * This query is a bit complex. It would be much easier to write this aggregation in Java code, but it's
+     * expensive (network, memory) to do that because it requires shipping all of the row data from the
+     * Postgres database into the Java process. For large data, that's too cost prohibitive. So this is an
+     * example that shows how to execute hand-written SQL queries in a Micronaut/Postgres context.
+     */
+    private static void adhocQuery(ApplicationContext ctx) throws SQLException {
+        log.info("Computing a word count...\n\n");
+        // This query flattens the "notes" array value across all rows and then splits each note into individual
+        // words. The "unnest" function flattens the arrays and then the "regexp_split_to_table" function splits the note
+        // strings into individual words. The result of this operation goes into a Common Table Expression (CTE) named
+        // "note_words". Then, a simple count aggregation is applied to the "note_words" table to get the final word
+        // count.
+        var sql = """
+                with note_words as (select regexp_split_to_table(unnest(notes), E'\\\\s+') word from observations),
+                word_count as (select word, count(*) count from note_words group by word)
+                select * from word_count order by count desc
+                """;
+
+        // This is how you get a reference to the DataSource object that you can use to make queries straight to the
+        // database. You need to "unwrap" the underlying DataSource from the top-level DataSource. If you don't, then
+        // the program will throw a "io.micronaut.transaction.exceptions.NoTransactionException" when you try to execute
+        // a query using the still-wrapped data source.
+        var dataSourceTopLevel = ctx.getBean(DataSource.class);
+        var dataSource = DelegatingDataSource.unwrapDataSource(dataSourceTopLevel);
+        var connection = dataSource.getConnection();
+        var statement = connection.createStatement();
+        var resultSet = statement.executeQuery(sql);
+
+        // Collect the results into a string that's formatted like a table. There are two columns: "Word" and "Count"
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("%-12s%sn%n", "Word", "Count")); // Header row
+        while (resultSet.next()) {
+            var word = resultSet.getObject(1);
+            var count = resultSet.getObject(2);
+            var formatString = "%-12s%s%n";
+            builder.append(String.format(formatString, word, count));
+        }
+        var wordCountTable = builder.toString();
+
+        log.info("Computed the following 'word count' across all words in the observation notes:\n\n{}", wordCountTable);
     }
 }
