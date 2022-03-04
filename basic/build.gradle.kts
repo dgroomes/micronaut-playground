7cfc94953c74e7c47d647456141ac3f0cd888df4
David Groomes <dwgroomes@gmail.com>
2022-03-03 22:51:58 -0600

    Remove workaround
    Thanks to https://github.com/micronaut-projects/micronaut-gradle-plugin/issues/385#event-6082333144

--- basic/build.gradle.kts
+++ basic/build.gradle.kts
@@ -2,7 +2,7 @@ import okhttp3.internal.cacheGet
 
 plugins {
     // Micronaut Gradle plugin releases: https://github.com/micronaut-projects/micronaut-gradle-plugin/releases
-    id("io.micronaut.application") version "3.2.1"
+    id("io.micronaut.application") version "3.2.2"
 }
 
 repositories {
@@ -31,16 +31,5 @@ application {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-tasks {
-    named<JavaExec>("run") {
-        // For some reason, the "run" task is being skipped on subsequent executions. Gradle's caching capability knows
-        // to skip tasks when their inputs haven't changed, like the dependency resolution and compile tasks. This is a
-        // good feature in general. But the "run" task is not a "building" task, it's a "running" task. It doesn't make
-        // sense for the run task to be "cached". When we want to run our program, we should be able to run our program.
-        // A workaround is to use the "outputs.upToDateWhen" configuration to force Gradle to never cache this task.
-        outputs.upToDateWhen { false }
     }
 }
--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Micronaut Gradle plugin releases: https://github.com/micronaut-projects/micronaut-gradle-plugin/releases
-    id("io.micronaut.application") version "3.2.1"
+    id("io.micronaut.application") version "3.2.2"
 }
 
 repositories {
@@ -29,16 +29,5 @@ application {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-tasks {
-    named<JavaExec>("run") {
-        // For some reason, the "run" task is being skipped on subsequent executions. Gradle's caching capability knows
-        // to skip tasks when their inputs haven't changed, like the dependency resolution and compile tasks. This is a
-        // good feature in general. But the "run" task is not a "building" task, it's a "running" task. It doesn't make
-        // sense for the run task to be "cached". When we want to run our program, we should be able to run our program.
-        // A workaround is to use the "outputs.upToDateWhen" configuration to force Gradle to never cache this task.
-        outputs.upToDateWhen { false }
     }
 }
