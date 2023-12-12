86419f211d722d2209a7249c54ba1c6cca68dca1
David Groomes <dwgroomes@gmail.com>
2023-12-12 01:03:48 -0600

    [`server-side-rendering/`] Upgrade to Micronaut 4.x

--- server-side-rendering/build.gradle.kts
+++ server-side-rendering/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Micronaut Gradle plugin releases: https://github.com/micronaut-projects/micronaut-gradle-plugin/releases
-    id("io.micronaut.application") version "3.2.2"
+    id("io.micronaut.application") version "4.2.1"
 }
 
 repositories {
@@ -8,7 +8,7 @@ repositories {
 }
 
 micronaut {
-    version("3.3.4") // Micronaut releases: https://github.com/micronaut-projects/micronaut-core/releases
+    version("4.2.1") // Micronaut releases: https://github.com/micronaut-projects/micronaut-core/releases
     testRuntime("junit5")
     processing {
         incremental(true)
@@ -17,21 +17,13 @@ micronaut {
 }
 
 dependencies {
-    // Micronaut projects like this one will print warnings during compilation like
-    //
-    //   warning: unknown enum constant When.MAYBE
-    //     reason: class file for javax.annotation.meta.When not found
-    //
-    // These warnings started with the release of Micronaut 3.0.0. To suppress the warnings you need to add a compile-only
-    // dependency on findbugs. See the note in the "Nullable Annotations" section under the "3.0.0" section under the
-    // "20.5 Breaking Changes" section in the Micronaut docs: https://docs.micronaut.io/latest/guide/#breaks
-    compileOnly("com.google.code.findbugs:jsr305")
-    implementation("io.micronaut:micronaut-validation")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut:micronaut-http-server-netty")
+    implementation("io.micronaut:micronaut-jackson-databind")
     implementation("io.micronaut.views:micronaut-views-thymeleaf")
     runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("org.yaml:snakeyaml")
 }
 
 application {
