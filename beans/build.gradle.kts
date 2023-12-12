9ae323c387fd70525fa47e00844054e650216b72
David Groomes <dwgroomes@gmail.com>
2023-12-12 00:42:40 -0600

    [`beans/`] Upgrade to Micronaut 4.x

--- beans/build.gradle.kts
+++ beans/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
     mavenCentral()
 }
 
-val micronautVersion = "3.3.4" // Micronaut releases: https://github.com/micronaut-projects/micronaut-core/releases
-val logbackVersion = "1.2.11" // Logback releases: http://logback.qos.ch/news.html
+val micronautVersion = "4.2.1" // Micronaut releases: https://github.com/micronaut-projects/micronaut-core/releases
+val logbackVersion = "1.4.14" // Logback releases: http://logback.qos.ch/news.html
 
 dependencies {
     annotationProcessor("io.micronaut:micronaut-inject-java:$micronautVersion")
